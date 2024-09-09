7f043d6a9bad21c361c11e51e81b9740e1956fb7
Hansen <300071283@fvtc.edu>
2024-09-09 14:50:27 -0500

    added time code and div with id of welcome to set up the homework

--- alexandershomepage/site.js
+++ alexandershomepage/site.js
@@ -1,1 +1,5 @@
-console.log('hello world')
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
