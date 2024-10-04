0512db9f39f874639c7cb704d8d792e99ecdee39
Alexander Hansen <300071283@fvtc.edu>
2024-10-04 04:56:28 -0500

    added new page to website, and added image carousel

--- alexandershomepage/site.js
+++ alexandershomepage/site.js
@@ -24,3 +24,42 @@ else if (isEvening) h3.textContent = `Good Evening its ${time.hour}:${time.minut
 const key = `It's a secret to everybody.`
 localStorage.setItem(`It's a secret to everybody.`, 'please please please buy our game.... From A5J studios when it releases at some point')
 
+//code for image carousel
+const nextButton = document.querySelector('#next')
+const previousButton = document.querySelector('#prev')
+
+const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+].map(url => { (new Image()).src = url; return url })
+
+const images = document.querySelectorAll('#carousel img')
+
+let currentImage = 0
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+}
+
+showImages()
+
+setInterval(() => {
+    currentImage++
+    showImages()
+}, 5000)
+
+nextButton.addEventListener('click' ,() => {
+    currentImage++
+    showImages()
+})
+
+previousButton.addEventListener('click' ,() => {
+    currentImage--
+    showImages()
+})
